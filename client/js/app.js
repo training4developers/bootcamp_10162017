98696f8282eb000c80410e1f213edb715b70ee53
Eric W. Greene <eric@ericwgreene.com>
2017-10-18 13:14:13 -0700

    added code demos

--- client/js/app.js
+++ client/js/app.js
@@ -3,7 +3,9 @@ import * as ReactDOM from 'react-dom';
 
 import { ColorTool } from './components/color-tool';
 
+const colors = ['red','white','gold','green','saffron','blue'];
+
 ReactDOM.render(
-  <ColorTool />,
-  document.querySelector('main')
+  <ColorTool colorList={colors} />,
+  document.querySelector('main'),
 );
--- client/js/components/color-tool.js
+++ client/js/components/color-tool.js
@@ -1,34 +1,43 @@
 import * as React from 'react';
 
-Exercise #2
-
-1. Add to the Car Tool, a table of cars
-
-2. Columns
-  Make
-  Model
-  Year
-  Color
-  Price
-
-3. Add two cars, and be sure to fill in of the columns.
-
-
 export class ColorTool extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newColorName: '',
+      newColorHexCode: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [ e.target.name ]: e.target.value });
+  }
+
   render() {
+
     return <div>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>red</li>
-        <li>white</li>
-        <li>gold</li>
-        <li>green</li>
-        <li>saffron</li>
-        <li>blue</li>
+        {this.props.colorList.map(color => <li>{color}</li>)}
       </ul>
+      <form>
+        <div>
+          <label htmlFor="new-color-name-input">New Color Name:</label>
+          <input type="text" id="new-color-name-input" name="newColorName"
+            value={this.state.newColorName} onChange={this.onChange} />
+        </div>
+        <div>
+          <label htmlFor="new-color-hex-code-input">New Color Hex Code:</label>
+          <input type="text" id="new-color-hex-code-input" name="newColotHexCode"
+            value={this.state.newColorHexCode} onChange={this.onChange} />
+        </div>
+      </form>
     </div>;
   }
 }
